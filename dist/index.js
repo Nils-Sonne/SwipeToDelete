079ef1f04433212593e4cdbfb591644f8fb6e898
Nils-Sonne <U2407197@RTS-365.DK>
2021-11-01 13:06:56 +0100

    add js

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var jokeItem = document.querySelector(".jokeItem");
+var touchCordinateStart;
+var touchCordinateMove;
+jokeItem.addEventListener("touchstart", function (e) {
+  touchCordinateStart = e.touches[0].clientX;
+});
+jokeItem.addEventListener("touchmove", function (e) {
+  touchCordinateMove = Math.floor(e.touches[0].clientX);
+
+  if (touchCordinateMove < touchCordinateStart) {
+    jokeItem.style.transform = "translateX(".concat(touchCordinateMove - touchCordinateStart, "px)");
+  }
+});
---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+
+
+const jokeItem = document.querySelector(".jokeItem");
+let touchCordinateStart;
+let touchCordinateMove;
+
+jokeItem.addEventListener("touchstart", (e) => {
+  touchCordinateStart = e.touches[0].clientX;
+});
+jokeItem.addEventListener("touchmove", (e) => {
+  touchCordinateMove = Math.floor(e.touches[0].clientX);
+  if (touchCordinateMove < touchCordinateStart) {
+    jokeItem.style.transform = `translateX(${
+      touchCordinateMove - touchCordinateStart
+    }px)`;
+  }
+});
