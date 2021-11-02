07446db4a7abf659c64e099a6df06d7333e423ec
Nils-Sonne <U2407197@RTS-365.DK>
2021-11-02 10:37:55 +0100

    Structure changes

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,33 @@
 var jokeItem = document.querySelector(".jokeItem");
 var touchCordinateStart;
 var touchCordinateMove;
+var touchCordinateEnd;
+var deleteButtonWidth = window.screen.width * 40 / 100;
+document.querySelector(".deleteItem").addEventListener("click", function () {
+  document.querySelector("section").classList.add("animate__fadeOutLeft");
+  setTimeout(function () {
+    document.querySelector("section").classList.add("collapsed");
+  }, 800);
+  setTimeout(function () {
+    document.querySelector("section").remove();
+  }, 900);
+});
 jokeItem.addEventListener("touchstart", function (e) {
   touchCordinateStart = e.touches[0].clientX;
 });
 jokeItem.addEventListener("touchmove", function (e) {
   touchCordinateMove = Math.floor(e.touches[0].clientX);
 
-  if (touchCordinateMove < touchCordinateStart) {
+  if (touchCordinateMove < touchCordinateStart && touchCordinateMove > touchCordinateStart - deleteButtonWidth) {
     jokeItem.style.transform = "translateX(".concat(touchCordinateMove - touchCordinateStart, "px)");
+  }
+});
+jokeItem.addEventListener("touchend", function (e) {
+  touchCordinateEnd = Math.floor(e.changedTouches[0].clientX);
+
+  if (touchCordinateEnd < touchCordinateStart - deleteButtonWidth / 2) {
+    jokeItem.style.transform = "translateX(-".concat(deleteButtonWidth, "px)");
+  } else {
+    jokeItem.style.transform = "translateX(".concat(e.target.offsetLeft, ")");
   }
 });
--- js/index.js
+++ js/index.js
@@ -1,18 +1,39 @@
 
-
-
-const jokeItem = document.querySelector(".jokeItem");
+let jokeItem = document.querySelector(".jokeItem");
 let touchCordinateStart;
 let touchCordinateMove;
+let touchCordinateEnd;
+let deleteButtonWidth = (window.screen.width * 40) / 100;
+
+document.querySelector(".deleteItem").addEventListener("click", () => {
+  document.querySelector("section").classList.add("animate__fadeOutLeft");
+  setTimeout(() => {
+    document.querySelector("section").classList.add("collapsed");
+  }, 800);
+  setTimeout(() => {
+    document.querySelector("section").remove();
+  }, 900);
+});
 
 jokeItem.addEventListener("touchstart", (e) => {
   touchCordinateStart = e.touches[0].clientX;
 });
 jokeItem.addEventListener("touchmove", (e) => {
   touchCordinateMove = Math.floor(e.touches[0].clientX);
-  if (touchCordinateMove < touchCordinateStart) {
+  if (
+    touchCordinateMove < touchCordinateStart &&
+    touchCordinateMove > touchCordinateStart - deleteButtonWidth
+  ) {
     jokeItem.style.transform = `translateX(${
       touchCordinateMove - touchCordinateStart
     }px)`;
+  }
+});
+jokeItem.addEventListener("touchend", (e) => {
+  touchCordinateEnd = Math.floor(e.changedTouches[0].clientX);
+  if (touchCordinateEnd < touchCordinateStart - deleteButtonWidth / 2) {
+    jokeItem.style.transform = `translateX(-${deleteButtonWidth}px)`;
+  } else {
+    jokeItem.style.transform = `translateX(${e.target.offsetLeft})`;
   }
 });
